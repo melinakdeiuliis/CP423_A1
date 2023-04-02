0a0c7f4de99cf1a025a5af89f916ea9f63afd1f2
ana-mali <92402688+ana-mali@users.noreply.github.com>
2023-04-01 20:53:07 -0400

    Update cluster_news.py

--- A3/cluster_news.py
+++ A3/cluster_news.py
@@ -1,22 +1,34 @@
 import argparse
-import string
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import nltk
-import urllib.request
+import re
 import tarfile
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans,AgglomerativeClustering,DBSCAN
+from sklearn.metrics import adjusted_mutual_info_score, adjusted_rand_score, completeness_score
+import numpy as np
 stopwords = set(stopwords.words('english'))
 
 # Define function to preprocess text
 def preprocess_text(text):
+    # If the input is a dictionary, get the text content
+    if isinstance(text, dict):
+        text = text.get('content', '')
+    
+    # Convert byte-like object to string
+    if isinstance(text, bytes):
+        text = text.decode('utf-8')
+    
     # Remove punctuation
-    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = re.sub(r'[^\w\s]', '', text)
     # Convert to lowercase
     text = text.lower()
     # Split into words
     words = nltk.word_tokenize(text)
     # Remove stop words
-    words = [word for word in words if word not in stopwords]
+    words = [word for word in words if word not in stopwords.words('english')]
     # Stem words
     stemmer = PorterStemmer()
     words = [stemmer.stem(word) for word in words]
@@ -39,45 +51,65 @@ def count_words(text):
 
 # Define function to represent documents as word frequency vectors
 def create_word_frequency_vectors(docs):
-    # Preprocess documents and count words
-    word_counts = [count_words(preprocess_text(doc)) for doc in docs]
-    # Create dictionary of unique words
-    unique_words = sorted(list(set(word for word_count in word_counts for word in word_count.keys())))
-
-    # Create word frequency vectors
-    vectors = []
-    for word_count in word_counts:
-        vector = [word_count.get(word, 0) for word in unique_words]
-        vectors.append(vector)
-
-    # Return word frequency vectors and unique words
-    return vectors, unique_words
+    vectors=[]
+    for doc in docs:
+        vectorizer = TfidfVectorizer()
+        tfidf_vectors = vectorizer.fit_transform(doc)
+        vectors.apppend(tfidf_vectors)
+    return vectors
 
 
-tar = tarfile.open("20_newsgroups.tar.gz", "r:gz")
 
-documents = []
-x=0
-for member in tar.getmembers():
-    if member.isfile():
-        f = tar.extractfile(member)
-        content = f.read()#.decode("utf-16le")
-        documents.append(content)
-        x+=1
-        
-print(x)
-tar.close()
 
-vectors, unique_words = create_word_frequency_vectors(documents)
-print('Unique words:', unique_words)
-print('Word frequency vectors:')
-for vector in vectors:
-    print(vector)
+def cluster_news(ncluster,kmeans,whc,ac,dbscan):
+
+    tar = tarfile.open("20_newsgroups.tar.gz", "r:gz")
+
+    documents = []
+    x=0
+    for member in tar.getmembers():
+        if member.isfile():
+            f = tar.extractfile(member)
+            content = f.read()
+            try:
+                # Try to decode the content as UTF-8
+                content = content.decode('utf-8')
+            except UnicodeDecodeError:
+                # If decoding as UTF-8 fails, try decoding as UTF-16LE
+                content = content.decode('utf-16le')
+            # Convert the decoded string to bytes using UTF-8 encoding
+            content = content.encode('utf-8')
+            documents.append(content)
+            x+=1
+            
+    print(x)
+    tar.close()
+
+    vectors = create_word_frequency_vectors(documents)
+    arr=np.array(vectors)
+    k=ncluster
+    if (kmeans==True):
+        kmeans = KMeans(k, random_state=0).fit(arr)
+        predicted_k=kmeans.labels_
+    if (whc==True):
+        ward=AgglomerativeClustering(k,linkage='ward').fit(arr) 
+        predicted_w=ward.labels_
+    if (ac==True):
+        aggl=AgglomerativeClustering(k).fit(arr)
+        predicted_a=aggl.labels_
+    if (dbscan==True):
+        dbscan = DBSCAN(eps=0.5, min_samples=2).fit(arr)
+        predicted_db=dbscan.labels_
+
+    #calculate ground_truth labels for each,calculate and print them 
+    #ami = adjusted_mutual_info_score(true_labels, predicted_labels)
+    #ars = adjusted_rand_score(true_labels, predicted_labels)
+    #completeness = completeness_score(true_labels, predicted_labels)
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--ncluster',type=bool,default=False),
-parser.add_argument('--kmeans', type=bool, default=False)
+parser.add_argument('--ncluster',type=int,default=20)
+parser.add_argument('--kmeans', type=bool, default=True)
 parser.add_argument('--whc', type=bool, default=False)
 parser.add_argument('--ac', type=bool, default=False)
 parser.add_argument('--dbscan',type=bool,default=False)
