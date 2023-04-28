8d9757e592d40215b1ae3e981494218d51367351
melinakdeiuliis <82980469+melinakdeiuliis@users.noreply.github.com>
2023-04-28 14:22:44 -0400

    Update searchengine.py

--- FinalProject/searchengine.py
+++ FinalProject/searchengine.py
@@ -13,6 +13,31 @@ import requests
 import hashlib
 inverted_index=None
 
+import requests
+from nltk.tokenize import word_tokenize
+import pandas as pd
+from bs4 import BeautifulSoup
+from string import punctuation
+import numpy as np
+import pandas as pd
+from string import punctuation
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import cross_validate
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import joblib
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('English'))
+punctuation = set(punctuation)
+
+'''
+#------------------------------------------------------------------------------------#
+Classes and Functions
+#------------------------------------------------------------------------------------#
+'''
 
 class InvertedIndex:
     def __init__(inverted_index):
@@ -36,6 +61,135 @@ class InvertedIndex:
         for term, code in soundex.Soundex().items():
             if code == soundex_code:
                 return term
+
+ classifier = MultinomialNB()
+
+def clean_data(text):
+    tokens = word_tokenize(text)
+    clean_tokens = [token.lower() for token in tokens if token.lower() not in stop_words and token.lower() not in punctuation and token.isalpha()]
+    return clean_tokens
+
+def topicContentAxes(file):
+
+    data_list = []
+
+    dataset = pd.read_csv('sources.txt', names = ['topic', 'text'], sep = ',')
+
+    i = 0
+    for link in dataset['text']:
+
+        response = requests.get(link)
+        content = response.text
+
+        soup = BeautifulSoup(content, 'html.parser')
+        
+        dataset['text'][i] = soup.get_text()
+        i += 1
+
+    dataset['text'] = dataset['text'].apply(clean_data)
+    #print(dataset['text'])
+
+    data_list.append(dataset)
+    dataset = pd.concat(data_list)
+
+    websites = dataset['text'].values
+    # Sorts the records by list of strings
+    x = [' ' .join(doc) for doc in websites]
+    y = dataset['topic'].values
+    
+    return x, y
+
+def classifier_Training(classifier, x, y):
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
+
+    vectorizer = CountVectorizer()
+    vectorizer.fit(x_train)
+
+    joblib.dump(vectorizer, 'vectorizer.model')
+    
+    X_training = vectorizer.transform(x_train)
+    X_testing = vectorizer.transform(x_test)
+
+    classifier.fit(X_training, y_train)
+
+    y_pred = classifier.predict(X_testing)
+
+    x = vectorizer.fit_transform(x)
+
+    scoring = {
+    'accuracy': make_scorer(accuracy_score),
+    'precision': make_scorer(precision_score, average='weighted'),
+    'recall': make_scorer(recall_score, average='weighted'),
+    'f1_score': make_scorer(f1_score, average='weighted')
+    }
+    
+    evaluation = cross_validate(classifier, x, y, scoring=scoring)
+
+    accuracy = evaluation['test_accuracy'].mean()
+    recall = evaluation['test_recall'].mean()
+    precision = evaluation['test_precision'].mean()
+    f1 = evaluation['test_f1_score'].mean()
+
+    #classifier.fit(X_training, y_train)
+    y_pred = classifier.predict(X_testing)
+
+    accuracy_test = accuracy_score(y_test, y_pred)
+    recall_test = recall_score(y_test, y_pred, average='weighted')
+    precision_test = precision_score(y_test, y_pred, average='weighted')
+    f1_test = f1_score(y_test, y_pred, average='weighted')
+    cm = confusion_matrix(y_test, y_pred)
+
+    data = {
+        "Cross-validation results": {
+            "Accuracy": accuracy,
+            "Recall": recall,
+            "Precision": precision,
+            "F1-score": f1
+        },
+        "Test results": {
+            "Accuracy": accuracy_test,
+            "Recall": recall_test,
+            "Precision": precision_test,
+            "F1-score": f1_test
+        }
+    }
+
+    # Save classifier model for predict_sentiment.py
+    joblib.dump(classifier, 'classifier.model')
+
+    return data, cm
+
+def evaluation_Results(data, cm):
+
+    col_width = 18
+
+    for title, values, in data.items():
+        print(f"{title.upper():^{col_width * 2}}")
+        print("-" * col_width * 2)
+        
+        # Print the section content
+        if isinstance(values, dict):
+            for k, v in values.items():
+                print(f"{k:<{col_width}}{v:>{col_width}}")
+        elif isinstance(values, list):
+            for row in values:
+                print("".join([f"{str(item):<{col_width}}" for item in row]))
+        print()
+
+    display_Confusion_Matrix(cm)
+
+def display_Confusion_Matrix(matrix):
+    display = ConfusionMatrixDisplay(confusion_matrix = matrix)
+    display.plot()
+    plt.show()           
+
+'''    
+#------------------------------------------------------------------------------------#
+Search Engine
+#------------------------------------------------------------------------------------#
+'''
+
 def printoptions():
     print("""Select an option:
 1- Collect new documents.
@@ -136,12 +290,44 @@ def searchengine():
             print("Top 3 document results")
             for x in results:
                 print(x)
+                
         elif(opt==4):
-            print('option 4')
+            classifier = MultinomialNB()
+            x, y = topicContentAxes('sources.txt')
+            data, cm = classifier_Training(classifier, x, y)
+            evaluation_Results(data, cm)
 
         elif(opt==5):
             link=input("Please enter a link: ")
-        
+            response = requests.get(link)
+            content = response.text
+            soup = BeautifulSoup(content, 'html.parser')
+
+            text = soup.get_text()
+
+            text_list = []
+
+            text = clean_data(text)
+
+            text_list.append(text)
+
+            x = [' ' .join(text)]
+
+            classifier = joblib.load('classifier.model')
+            vectorizer = joblib.load('vectorizer.model')
+
+            X_testing = vectorizer.transform(x)
+
+            label_encoder = LabelEncoder()
+            label_encoder.classes_ = classifier.classes_
+
+            y_pred = classifier.predict(X_testing)
+
+            #confidence = classifier.predict_proba(X_testing)[0][y_pred]
+            print(y_pred)
+            # Print the predicted label and its confidence score
+            print("Predicted label: ", label_encoder.inverse_transform(y_pred))
+            #print("Confidence score: ", confidence)
 
 
         elif(opt==6):
